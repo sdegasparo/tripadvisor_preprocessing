671a5de96c79903aaffb091d45360800a2550b76
Sven De Gasparo <sven.degasparo@hispeed.ch>
2022-09-20 18:26:06 +0200

    Create script to export the database to a csv file

---
+++ export_db.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pandas as pd
+
+def main():
+    conn = sqlite3.connect('hotel_swiss.db')
+
+    df_hotels = pd.read_sql_query("SELECT * FROM hotels", conn)
+    df_reviewers = pd.read_sql_query("SELECT * FROM reviewers", conn)
+    df_reviews = pd.read_sql_query("SELECT * FROM reviews", conn)
+
+    df_reviews_reviewers = df_reviews.merge(df_reviewers, left_on='username_id', right_on='username_id')
+    df = df_reviews_reviewers.merge(df_hotels, left_on='hotel_id', right_on='hotel_id')
+
+    df.to_csv('hotels.csv', index=False)
+
+if __name__ == '__main__':
+    main()
